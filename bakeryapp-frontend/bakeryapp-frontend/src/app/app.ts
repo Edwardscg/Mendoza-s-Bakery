95a6ee54fedcdb699928f8e0c941d62c71758466
Dante7481 <mirkoschunque@gmail.com>
2026-05-09 21:06:21 -0500

    modelo productCategory

--- bakeryapp-frontend/bakeryapp-frontend/src/app/app.ts
+++ bakeryapp-frontend/bakeryapp-frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { CustomerComponent } from './pages/customer/customer.component';
 import { SupplierComponent } from './pages/supplier/supplier.component';
-
-
+import { ProductCategoryComponent } from './pages/product-category/product-category.component';
 @Component({
   selector: 'app-root',
-  imports: [CustomerComponent, SupplierComponent],
+  imports: [CustomerComponent, SupplierComponent, ProductCategoryComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
