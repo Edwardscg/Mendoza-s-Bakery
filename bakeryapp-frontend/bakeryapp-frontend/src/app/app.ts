3da171599d476548c314176d7a9bdffbd228b056
Edward <Edward@AN515-57>
2026-05-14 20:04:18 -0500

    feat: agregar layout component

--- bakeryapp-frontend/bakeryapp-frontend/src/app/app.ts
+++ bakeryapp-frontend/bakeryapp-frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { CustomerComponent } from './pages/customer/customer.component';
 import { SupplierComponent } from './pages/supplier/supplier.component';
 import { EmployeeComponent } from './pages/employee/employee.component';
 import { ProductCategoryComponent } from './pages/product-category/product-category.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CustomerComponent, SupplierComponent, ProductCategoryComponent, EmployeeComponent],
+  imports: [LayoutComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
